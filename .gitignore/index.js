00800ad3eae9356678318fc168a7be4f8532fc2b
Steellgold <38256643+SteellgoldEstFrancais@users.noreply.github.com>
2018-04-11 18:37:55 +0200

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var prefix = ("h!");
+  
+
+bot.login(process.env.TOKEN)
+console.log("Ready to Start Utulisation")
+
+bot.on('ready', function (){
+    bot.user.setPresence({ game: { name: "| h!help | HyperiaNetwork |"}})
+})
+
+bot.on("guildMemberAdd", member => {
+  member.guild.channels.find("name", "endvonia").send(`__Bonjour !__ ${member} \n \n**Bienvenue sur HyperiaNetwork** ! \n \n*Besoins d'aide ? =>* \nMentione le **Staff** `)
+  console.log("Quelqu'un a rejoint le Discord")
+})
+
+bot.on("guildMemberRemove", member => {
+  member.guild.channels.find("name", "endvonia").send(`Aurevoir ! ${member}`)
+  console.log("Quelqu'un a quitté le Discord")
+})
+
+bot.on('message', message => {
+    
+    if (message.content === (prefix + "help")){
+        var help_embed = new Discord.RichEmbed()
+           .setColor('#269f26')
+            .addField("__Developement__", "*Desolé Steellgold me Dévelope*")
+           .setFooter("Developed by Steellgold")        
+           message.channel.send(help_embed)
+           console.log("ed!help")
+    }
+})
