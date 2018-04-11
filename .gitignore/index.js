bf89e8635b02cf56b2d3e4e0a211e68bedfd9b56
Steellgold <38256643+SteellgoldEstFrancais@users.noreply.github.com>
2018-04-11 20:40:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -32,4 +32,36 @@ bot.on('message', message => {
            message.channel.send(help_embed)
            console.log("ed!help")
     }
+  
+  //commande ban
+if(message.content === prefix + 'ban'){
+    let raison = message.content.substr(27);
+    let banMember = message.guild.member(message.mentions.users.first());
+    if(!message.member.hasPermissions("BAN_MEMBERS")){
+        message.channel.send(`❌ ${message.author},Tu n'as pas la permission de ban les chatons.`)
+        return;
+    }else{
+        if(!banMember){
+            message.channel.send(`❌ ${message.author}, veuillez mentionner un chaton.`)
+            return;
+      }else{                                                 
+          if(!banMember.bannable){
+              message.channel.send(`❌ ${message.author},Je peux pas ban ce chaton veuillez vérifier mes rôles et permissions`)
+              return;
+        }else{                                                 
+            if(banMember.hasPermission("ADMINISTRATOR")){
+                message.channel.send(`❌ ${message.author},ce chaton est trop puissant, je peux pas faire ça`)
+                return;
+            }else{
+    message.delete(message.author);
+    message.guild.member(banMember).ban({reason: `${raison}`});
+    message.channel.sendMessage({embed:{color: `${banMember} a été ban ! raison: ${raison}`}});
+            }
+          }
+        }
+    }
+  }
+
+  
+})
 })
